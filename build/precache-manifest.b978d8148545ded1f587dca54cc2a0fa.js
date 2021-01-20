ac8c5399ba4aa0b2361935225f6542eec7263956
BKMMYTG <yilmaz.murat@tcmb.gov.tr>
2021-01-20 22:22:34 +0300

    Birth place fields added.

---
+++ build/precache-manifest.b978d8148545ded1f587dca54cc2a0fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5360172b5abc0d5e2a8a",
+    "url": "/static/css/main.88d5f900.chunk.css"
+  },
+  {
+    "revision": "5360172b5abc0d5e2a8a",
+    "url": "/static/js/main.bfdf171c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "370a3727f0bf8831d519",
+    "url": "/static/css/2.9e5a1fb5.chunk.css"
+  },
+  {
+    "revision": "370a3727f0bf8831d519",
+    "url": "/static/js/2.d3a9d5af.chunk.js"
+  },
+  {
+    "revision": "487638e5dae94de7ac2b2e3b4713f403",
+    "url": "/index.html"
+  }
+];
